70d38662670063cac401ef439cfa9bbbabdc716e
Dymsi <30205360+Dymsi@users.noreply.github.com>
2018-01-24 03:36:46 +0000

    Create recursos.java

---
+++ core/recursos.java
@@ -0,0 +1,54 @@
+package core;
+
+
+public class recursos
+{
+    // <editor-fold defaultstate="collapsed" desc="Variaveis.">
+    private String edificio;
+    private int capacidadeMaxima;
+    private int ocupacaoAtual;
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Construtor.">
+    public recursos(String edificio, int capMax)
+    {
+        this.edificio = edificio;
+        this.capacidadeMaxima = capMax;
+        this.ocupacaoAtual = 0;
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Gets.">
+    public int getCapMax()
+    {
+        return this.capacidadeMaxima;
+    }
+    
+    public int getOcupacaoAtual()
+    {
+        return this.ocupacaoAtual;
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Sets.">
+    public void setCapMax(int x)
+    {
+        this.capacidadeMaxima = x;
+    }
+    
+    public void setOcupacaoAtual(int x)
+    {
+        this.ocupacaoAtual = x;
+    }
+    
+    public void adiOcupacaoAtual(int x)
+    {
+        this.ocupacaoAtual += x;
+    }
+    
+    public void subOcupacaoAtual(int x)
+    {
+        this.ocupacaoAtual -= x;
+    }
+    // </editor-fold>
+}
